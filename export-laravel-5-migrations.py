a22ba8c3f928dab0632f4c6d67ede21286466163
Brandon Eckenrode <beckenrode@gmail.com>
2016-06-22 09:05:45 -0400

    Added support for rememberToken()

--- export-laravel-5-migrations.py
+++ export-laravel-5-migrations.py
@@ -114,7 +114,9 @@ def generateLaravel5Migration(cat):
                     if col.length > -1:
                         col_data = '\', %s' % (str(col.length))
 
-                if(typesDict[col_type]) :
+                if col.name == 'remember_token' and typesDict[col_type] == 'string' and str(col.length) = 100:
+                    migrations[tbl.name].append('            $table->rememberToken();\n'
+                elif(typesDict[col_type]) :
                     migrations[tbl.name].append('            $table->%s(\'%s%s)' % (typesDict[col_type], col.name, col_data))
                     if typesDict[col_type] == 'integer' and 'UNSIGNED' in col.flags:
                         migrations[tbl.name].append('->unsigned()')
